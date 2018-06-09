89405b362a291e0b9d4a3162d4116e4e1f08f976
jack <shzhaohao@gmail.com>
2018-06-09 21:40:56 +0800

    add mode_to_letter

--- ls/ls.c
+++ ls/ls.c
@@ -5,8 +5,42 @@
 #include<dirent.h>
 
 
+char* mode_to_letters(mode_t mode){
+    static char letters[10];
+    for(int i = 0; i < 10; i ++)
+        letters[i] = '-';
+
+    if(S_ISDIR(mode))
+        letters[0] = 'd';
+
+    if(S_IRUSR &mode)
+        letters[1] = 'r';
+    if(S_IWUSR &mode)
+        letters[2] = 'w';
+    if(S_IXUSR &mode)
+        letters[3] = 'x';
+
+    if(S_IRGRP &mode)
+        letters[4] = 'r';
+    if(S_IWGRP &mode)
+        letters[5] = 'w';
+    if(S_IXGRP &mode)
+        letters[6] = 'x';
+
+    if(S_IROTH &mode)
+        letters[7] = 'r';
+    if(S_IWOTH &mode)
+        letters[8] = 'w';
+    if(S_IXOTH &mode)
+        letters[9] = 'x';
+
+    return letters;
+
+}
+
+
 void show_file_info(struct stat* statp, const char* filename){
-    printf("mode: %o\t",statp->st_mode);
+    printf("%s\t", mode_to_letters(statp->st_mode));
     printf("link: %lu\t",statp->st_nlink);
     printf("uid: %d\t",statp->st_uid);
     printf("gid: %d\t",statp->st_gid);
@@ -15,6 +49,9 @@ void show_file_info(struct stat* statp, const char* filename){
     printf("name: %s\n",filename);
 
 }
+
+
+
 
 int do_ls(const char* pathname){
     struct stat path_stat;
@@ -33,8 +70,12 @@ int do_ls(const char* pathname){
         if(  (dirp = opendir(pathname)) == NULL)
             perror("opendir(pathname) failed\n");
         struct dirent *dire;
+        struct stat file_stat;
         while( (dire = readdir(dirp)) != NULL){
-            show_file_info(&path_stat, dire->d_name);        
+            if(stat(dire->d_name, &file_stat)){
+                perror("stat(dire->d_name) failed\n");
+            }
+            show_file_info(&file_stat, dire->d_name);        
         }
     }
 
